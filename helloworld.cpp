e942e973cfc65c36a9ca61527cd10ffbde5024e6
Harshini <hpadala@umich.edu>
2024-01-05 21:29:23 -0500

    hello world program

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,5 +1,5 @@
 #include <iostream> //file that has print funtionality in it
-using namespace std; //clarifying which namespace
+using namespace std; //clarifying which namespace to use
 
 int main() {
     //prints "Hello World"
